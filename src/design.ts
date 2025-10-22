58edeb4bb7ac3b8db89652ad7ca3d99238ffaa7d
Jun Wang <wj918@hotmail.com>
2025-10-21 23:49:22 -0400

    saved

--- src/design.ts
+++ src/design.ts
@@ -2,3 +2,12 @@
 import './card.css'
 import './style.css'
 import './design.css'
+
+/*
+const startBtn = document.getElementById('startBtn')! as HTMLInputElement
+startBtn!.addEventListener('click', async () => {
+    document.querySelector('.trick-0')!
+        .setAttribute('style', 'animation: move-to-location 500ms ease-in-out forwards;');
+
+});
+*/
